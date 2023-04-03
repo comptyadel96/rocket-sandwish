0d6b329ab297a492172a5552b78c6b57ce673fe9
boullif adel <adoulation2007@hotmail.com>
2023-04-03 11:27:24 +0200

    ffff

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  i18n: { locales: ["fr", "ar"], defaultLocale: "fr" },
+  i18n: {
+    locales: ["fr", "ar"],
+    defaultLocale: "fr",
+    domains: [
+      {
+        domain: "https://rocket-sandwish-2.vercel.app",
+        defaultLocale: "fr",
+      },
+      {
+        domain: "https://rocket-sandwish-2.vercel.app/Login",
+        defaultLocale: "fr",
+      },
+      {
+        domain: "https://rocket-sandwish-2.vercel.app/contact",
+        defaultLocale: "fr",
+        http: true,
+      },
+    ],
+  },
 }
